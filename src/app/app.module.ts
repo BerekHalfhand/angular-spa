9f28a641da65689553c84ce1f91f37246b593125
Moorindal <moorindal@ymail.com>
2019-02-12 16:41:21 -0500

    Flex markup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppComponent } from './app.component';
 import { ImageComponent } from './image/image.component';
@@ -9,7 +10,7 @@ import { ImageComponent } from './image/image.component';
 import { ImageService } from './image/image.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule, FlexLayoutModule ],
   declarations: [ AppComponent, ImageComponent ],
   bootstrap:    [ AppComponent ],
   providers:    [ ImageService ]
